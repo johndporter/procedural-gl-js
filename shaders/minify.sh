090fceded7aa2e275356e886cf0fe6c62f954e65
John <john.d.porter@gmail.com>
2021-01-04 17:08:16 +0000

    Use simpler syntax for glslmin reading.
    Originally minify.sh didn't work on linux.

--- shaders/minify.sh
+++ shaders/minify.sh
@@ -42,31 +42,31 @@ glsl-validate.py --write terrainPicker.frag
 glsl-validate.py --write terrainPicker.vert
 
 # Minify complete shaders
-glslmin beacon.full.frag -o min/beacon.frag
-glslmin beacon.full.vert -o min/beacon.vert
-glslmin line.full.frag -o min/line.frag
-glslmin line.full.vert -o min/line.vert
-glslmin marker.full.frag -o min/marker.frag
-glslmin marker.full.vert -o min/marker.vert
-glslmin picker.frag -o min/picker.frag
-glslmin picker.full.vert -o min/picker.vert
-glslmin raycast.frag -o min/raycast.frag
-glslmin raycast.full.vert -o min/raycast.vert
+glslmin < beacon.full.frag -o min/beacon.frag
+glslmin < beacon.full.vert -o min/beacon.vert
+glslmin < line.full.frag -o min/line.frag
+glslmin < line.full.vert -o min/line.vert
+glslmin < marker.full.frag -o min/marker.frag
+glslmin < marker.full.vert -o min/marker.vert
+glslmin < picker.frag -o min/picker.frag
+glslmin < picker.full.vert -o min/picker.vert
+glslmin < raycast.frag -o min/raycast.frag
+glslmin < raycast.full.vert -o min/raycast.vert
 
 # Sky
-glslmin sky.full.frag -o min/sky.frag
-glslmin sky.vert -o min/sky.vert
-glslmin skybox.full.frag -o min/skybox.frag
-glslmin skySpheremap.full.frag -o min/skySpheremap.frag
+glslmin <sky.full.frag -o min/sky.frag
+glslmin <sky.vert -o min/sky.vert
+glslmin <skybox.full.frag -o min/skybox.frag
+glslmin <skySpheremap.full.frag -o min/skySpheremap.frag
 
 # Postprocess
-glslmin postprocess.frag -o min/postprocess.frag
-glslmin quad.vert -o min/quad.vert
-glslmin quad_uv.vert -o min/quad_uv.vert
+glslmin <postprocess.frag -o min/postprocess.frag
+glslmin <quad.vert -o min/quad.vert
+glslmin <quad_uv.vert -o min/quad_uv.vert
 
 # Terrain
-glslmin terrain.full.frag -o min/terrain.frag
-glslmin terrain.full.vert -o min/terrain.vert
-glslmin terrainPicker.full.vert -o min/terrainPicker.vert
-glslmin terrainPicker.full.frag -o min/terrainPicker.frag
+glslmin <terrain.full.frag -o min/terrain.frag
+glslmin <terrain.full.vert -o min/terrain.vert
+glslmin <terrainPicker.full.vert -o min/terrainPicker.vert
+glslmin <terrainPicker.full.frag -o min/terrainPicker.frag
 echo "Shaders minified"
